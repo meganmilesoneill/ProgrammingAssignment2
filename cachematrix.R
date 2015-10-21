445407f4c9bc2a644eb6db57132bec565052335f
meganmilesoneill <meganmilesoneill@gmail.com>
2015-10-21 16:10:22 -0700

    Implemented the solution to Programming Exercise 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The makeCacheMatrix function is reponsible for taking a standard matrix
+## and providing functions for getting/setting the matrix and its inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # i will store the inverted matrix
+    i <- NULL
+    
+    # setter function to set the matrix
+    set <- function(y) {
+        x <<- y
+        # whenever we set the matrix, 
+        # we need to reset the inverted matrix
+        # so that it will be recalculated the next time
+        # the inverted matrix is accessed
+        i <<- NULL
+    }
+    # getter function to return the matrix
+    get <- function() x
+    # setter function for the solved/inverted the matrix
+    setsolve <- function(solve) i <<- solve
+    # getter function to return the solved/inverted matrix
+    getsolve <- function() i
+    # list exposing the internal functions of the special "matrix" object
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve )
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function is responsible for checking 
+## to see if a matrix has already been inverted and cached.
+## If it has not yet been inverted, it creates and caches
+## the inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getsolve()
+        
+        # check to see if the matrix has already been inverted and cached
+        if(!is.null(i)){
+            message("getting cached data")
+            return(i)
+        }
+        # matrix has not yet been inverted and cached, so we'll do that now
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setsolve(i)
+        # return the inverted matrix
+        i
 }
